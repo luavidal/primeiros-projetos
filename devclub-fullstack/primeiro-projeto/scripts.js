d26bb9d6022a48778c9215faff89c846f7e14b51
luavidal <l.maximiano.lmv@gmail.com>
2026-05-19 23:00:39 -0300

    Primeiro commit - Projeto Responsividade

---
+++ devclub-fullstack/primeiro-projeto/scripts.js
@@ -0,0 +1,10 @@
+//alert("Helo, world");
+
+let circulo = document.querySelector(".circulo");
+let copo = document.querySelector(".starbucks");
+
+function mudarCor(cor, imagem) {
+    circulo.style.backgroundColor = cor;
+    copo.scr = imagem;
+    }
+
