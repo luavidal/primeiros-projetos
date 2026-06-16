12807a661776a8d4a4b0558c2547526335ca4827
luavidal <l.maximiano.lmv@gmail.com>
2026-06-16 12:23:58 -0300

    06-16

--- Commit/scripts.js
+++ Commit/scripts.js
@@ -1,6 +1,20 @@
-let name = "Luã";
-if (name === "Luã"){
-    console.log(`Olá, ${name}!`);
-} else {
-    console.log("Olá, visitante!");
+const input = document.querySelector("input");
+let mensagem = document.querySelector("#mensagem");
+
+function DigiteiAqui () {
+    console.log("Digitei no input!");
+}
+
+function cliqueiNoBotao() {
+    if (input.value !== "") {
+        alert(input.value);
+        const text = `Olá, ${input.value}! Seja bem-vindo ao DevClub!`;
+        mensagem.innerHTML = text;
+    }else{
+        alert("Por favor, digite seu nome primeiro!");
+    }
+}
+
+function registrar() {
+
 }
